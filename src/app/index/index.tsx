1b5acb7af52eb7aed28f95582ffb5bf860a68895
moacirrochadev <empreendedormoacirneto@gmail.com>
2026-07-22 01:31:53 -0300

    Abrindo o modal de forma dinâmica

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -11,6 +11,8 @@ import { Alert, FlatList, Image, Modal, Text, TouchableOpacity, View } from "rea
 
 
 export default function Index() {
+    const [showModal, setShowModal] = useState(false)
+    const [link, setLink] = useState<LinkStorage>({} as LinkStorage)
     const [links, setLinks] = useState<LinkStorage[]>([])
     const [category, setCategory] = useState("categories[0].name")
 
@@ -24,6 +26,11 @@ export default function Index() {
         } catch (error) {
             Alert.alert("Erro", "Não foi possível carregar os links")
         }
+    }
+
+    function handleDetails(selected: LinkStorage) {
+        setShowModal(true)
+        setLink(selected)
     }
 
     useFocusEffect(
@@ -51,7 +58,7 @@ export default function Index() {
                     <Link
                         name={item.name}
                         url={item.url}
-                        onDetails={() => console.log("Clicou!")}
+                        onDetails={() => handleDetails(item)}
                     />
                 )}
                 style={styles.links}
@@ -59,18 +66,18 @@ export default function Index() {
                 showsVerticalScrollIndicator={false}
             />
 
-            <Modal transparent visible={false}>
+            <Modal transparent visible={showModal} animationType="fade">
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         <View style={styles.modalHeader}>
-                            <Text style={styles.modalCategory}>Curso</Text>
-                            <TouchableOpacity>
+                            <Text style={styles.modalCategory}>{link.category}</Text>
+                            <TouchableOpacity onPress={() => setShowModal(false)}>
                                 <MaterialIcons name="close" size={20} color={colors.gray[400]} />
                             </TouchableOpacity>
                         </View>
 
-                        <Text style={styles.modalLinkName}>Rocketseat</Text>
-                        <Text style={styles.modalUrl}>https://instagram.com</Text>
+                        <Text style={styles.modalLinkName}>{link.name}</Text>
+                        <Text style={styles.modalUrl}>{link.url}</Text>
 
                         <View style={styles.modalFooter}>
                             <Option name="Excluir" icon="delete" variant="secondary" />
